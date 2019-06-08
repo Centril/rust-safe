91f688a167dfa8b4d06f613be23f3ffb0cb0ceb8
Michael Bryan <michaelfbryan@gmail.com>
2019-06-08 22:47:44 +0800

    Moved the basic usage example into crate-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,61 @@
+//! A custom attribute for declaring why an `unsafe` block is correct.
+//!
+//! # Examples
+//!
+//! The most basic way this attribute can be used is by annotating an `unsafe`
+//! block and providing a `reason` for why it is valid.
+//!
+//! ```rust
+//! # #![feature(stmt_expr_attributes, proc_macro_hygiene)]
+//! # use safe::safe;
+//! let mut x: u32 = 42;
+//! let x_ptr = &mut x as *mut u32;
+//!
+//! #[safe(reason = "This is the only reference to x")]
+//! unsafe {
+//!     x_ptr.write(7);
+//! }
+//! ```
+//!
+//! Unsafe code often has constraints that must be valid before or after the
+//! code is run.
+//!
+//! You can provide an assertion which will be executed with `debug_assert!()`
+//! immediately before the block executes by adding a `requires = "..."` to
+//! the attribute.
+//!
+//! ```rust
+//! # #![feature(stmt_expr_attributes, proc_macro_hygiene)]
+//! # use safe::safe;
+//! let x: u32 = 42;
+//! let x_ptr = &x as *const u32;
+//!
+//! #[safe(
+//!     reason = "The pointer points to a valid integer",
+//!     requires = "!x_ptr.is_null()"
+//! )]
+//! unsafe {
+//!     assert_eq!(42, x_ptr.read());
+//! }
+//! ```
+//!
+//! Likewise, for conditions which must be upheld after the `unsafe` block, you
+//! can provide an `ensures` assertion.
+//!
+//! ```rust
+//! # #![feature(stmt_expr_attributes, proc_macro_hygiene)]
+//! # use safe::safe;
+//! let mut allocated: *mut u32;
+//!
+//! #[safe(
+//!     reason = "Malloc will always return a non-null pointer unless there is an out-of-memory error",
+//!     ensures = "!allocated.is_null()"
+//! )]
+//! unsafe {
+//!     allocated = libc::malloc(std::mem::size_of::<u32>()) as *mut u32;
+//! }
+//! ```
+
 extern crate proc_macro;
 
 use darling::FromMeta;
@@ -5,6 +63,10 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, AttributeArgs, Expr, ExprUnsafe, LitStr};
 
+/// A custom attribute for explaining why an `unsafe` block is valid and any
+/// invariants which must be upheld.
+///
+/// See crate-level docs for example usage.
 #[proc_macro_attribute]
 pub fn safe(attr: TokenStream, item: TokenStream) -> TokenStream {
     let attr_args = parse_macro_input!(attr as AttributeArgs);
